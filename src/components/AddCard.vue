a810612c16917ce8d1205b47da567206eccc83b4
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-09-14 16:38:32 -0400

    add routing to the application and create dummy components for adding and deleting cards

---
+++ src/components/AddCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header></Header>
+    <div>TODO style n content</div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+
+export default {
+
+    name: 'AddCard',
+    components: {
+        Header,
+        Footer,
+    }
+};
+</script>
---
+++ src/components/DeleteCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header></Header>
+    <div>TODO style n content</div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+
+export default {
+
+    name: 'DeleteCard',
+    components: {
+        Header,
+        Footer,
+    }
+};
+</script>
